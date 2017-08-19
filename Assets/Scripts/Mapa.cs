4431f0880178ec7d53d870687a05a31d78bb814d
Naril <narilgvb@gmail.com>
2017-08-19 21:29:01 +0200

    Base genetic generation.

---
+++ Assets/Scripts/Mapa.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Mapa {
+
+    //Variables
+
+    int[,] mapa;
+    int alto, ancho;
+    public string seed;
+    int bordesPost = 0;
+    int dentroPost = 0;
+    int fueraPost = 0;
+    int puentes = 0;
+    double calidad;
+
+    //Inicialización
+
+    public Mapa()
+    {
+        alto = 50;
+        ancho = 50;
+        mapa = new int[alto, ancho];
+    }
+
+    public Mapa(int alto, int ancho)
+    {
+        this.alto = alto;
+        this.ancho = ancho;
+        mapa = new int[alto, ancho];
+        this.ObtenerCalidad();
+    }
+
+    public Mapa(int alto, int ancho, int porcentajeRellenado)
+    {
+        this.alto = alto;
+        this.ancho = ancho;
+        GenerateRandomMap(porcentajeRellenado);
+        this.ObtenerCalidad();
+    }
+
+    public Mapa(int[,] mapa)
+    {
+        alto = mapa.GetLength(0);
+        ancho = mapa.GetLength(1);
+        this.mapa = new int[alto, ancho];
+        this.mapa = (int[,])mapa.Clone();
+        this.ObtenerCalidad();
+    }
+
+    //Setters y Getters
+
+    public int[,] GetMapa()
+    {
+        return mapa;
+    }
+
+    public int getPuntoMapa(int alto, int ancho)
+    {
+        return mapa[alto, ancho];
+    }
+
+    public void setPuntoMapa(int alto, int ancho, int punto)
+    {
+        mapa[alto, ancho] = punto;
+    }
+    
+    public void setAlto(int alto)
+    {
+        this.alto = alto;
+    }
+
+    public void setAncho(int ancho)
+    {
+        this.ancho = ancho;
+    }
+
+    public void setDimensiones(int x, int y)
+    {
+        setAlto(y);
+        setAncho(x);
+    }
+
+    public int getAlto()
+    {
+        return alto;
+    }
+
+    public int getAncho()
+    {
+        return ancho;
+    }
+
+    public double getCalidad()
+    {
+        return calidad;
+    }
+
+    //Manipulación del mapa
+
+    public void GenerateRandomMap(int porcentajeRellenado)
+    {
+        mapa = new int[alto, ancho];
+        seed = Time.realtimeSinceStartup.ToString();
+        System.Random randomNum = new System.Random(seed.GetHashCode());
+        for (int i = 0; i < alto; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                mapa[i, j] = randomNum.Next(0, 100) < porcentajeRellenado ? 1 : 0;
+            }
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            CorregirMapa();
+        }
+        AnalizarMapa();
+        ObtenerCalidad();
+    }
+
+    public void CorregirMapa()
+    {
+        for (int i = 0; i < alto; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                int numVecinos = Vecinos(i, j);
+                if (numVecinos > 4)
+                {
+                    mapa[i, j] = 1;
+                }
+                else if (numVecinos < 4)
+                    mapa[i, j] = 0;
+            }
+        }
+    }
+
+    void AnalizarMapa()
+    {
+        dentroPost = 0;
+        fueraPost = 0;
+        bordesPost = 0;
+        for (int i = 0; i < alto; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                int numVecinos = Vecinos(i, j);
+                if (mapa[i, j] == 1 && numVecinos == 8)
+                    dentroPost++;
+                else if (mapa[i, j] == 0)
+                {
+                    fueraPost++;
+                    if (numVecinos == 2)
+                        puentes++;
+                }
+                else if (numVecinos != 0 && numVecinos != 8 && mapa[i, j] == 1)
+                    bordesPost++;
+            }
+        }
+    }
+
+    public double ObtenerCalidad()
+    {
+        AnalizarMapa();
+
+        double d;
+        double calidadDentro;
+        double calidadFuera;
+        double calidadBordes;
+
+        d = Math.Abs(dentroPost - (alto * ancho * 0.65));
+        calidadDentro = 1 / (1 + d);
+        d = Math.Abs(fueraPost - (alto * ancho * 0.20));
+        calidadFuera = 1 / (1 + d);
+        d = Math.Abs(bordesPost - (alto * ancho * 0.15));
+        calidadBordes = 1 / (1 + d);
+
+        return calidad = calidadDentro + calidadFuera + calidadBordes - 1/puentes;
+    }
+
+    int Vecinos(int y, int x)
+    {
+        int numVecinos = 0;
+        for (int i = y - 1; i <= y + 1; i++)
+        {
+            for (int j = x - 1; j <= x + 1; j++)
+            {
+                if (i >= 0 && j >= 0 && i < alto && j < ancho)
+                {
+                    if (i != y || j != x)
+                    {
+                        numVecinos += mapa[i, j];
+                    }
+                }
+            }
+        }
+        return numVecinos;
+    }
+}
---
+++ Assets/Scripts/ReproducirMatrices.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ReproducirMatrices : MonoBehaviour {
+
+    Mapa geneticMap;
+    public int alto;
+    public int ancho;
+    public int porcentajeRellenado;
+    public int posMut;
+    public int posCru;
+    public GameObject inTile;
+    public GameObject outTile;
+    System.Random randomNum;
+    public int geneticRounds;
+
+    // Use this for initialization
+    void Start () {
+        geneticMap = new Mapa();
+        geneticMap = GenerateGeneticMap();
+        print("Final map quality: " + geneticMap.getCalidad());
+	}
+
+    Mapa GenerateGeneticMap()
+    {
+        Mapa map;
+        Mapa[] mapas = new Mapa[4];
+        for(int i = 0; i < 4; i++)
+        {
+            mapas[i] = new Mapa(alto, ancho, porcentajeRellenado);
+            print(i+1 + " map quality: " + mapas[i].getCalidad());
+        }
+        map = new Mapa(alto, ancho, porcentajeRellenado);
+
+        Mapa mapAux;
+
+        string seed = Time.realtimeSinceStartup.ToString();
+        randomNum = new System.Random(seed.GetHashCode());
+
+        double calidadTotal = 0f;
+        double[] calidadesPor100 = new double[4];
+
+        for (int k = 0; k < geneticRounds; k++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                calidadTotal += mapas[i].getCalidad();
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                calidadesPor100[i] = (mapas[i].getCalidad() * 100) / calidadTotal;
+                if (i > 0)
+                    calidadesPor100[i] += calidadesPor100[i - 1];
+            }
+
+            Mapa[] hijos = new Mapa[4];
+
+            for (int i = 0; i < 4; i+=2) {
+                int numRepro = randomNum.Next(1, 100);
+                int j;
+                for (j = 0; calidadesPor100[j] <= numRepro && j < 3; j++) ;
+                hijos[i] = new Mapa(mapas[j].GetMapa());
+
+                numRepro = randomNum.Next(1, 100);
+                for (j = 0; calidadesPor100[j] <= numRepro && j < 3; j++) ;
+                hijos[i+1] = new Mapa(mapas[j].GetMapa());
+
+                ReprodudirMapas(hijos[i], hijos[i+1]);
+            }
+
+            mapAux = mapas[0];
+            for (int i = 1; i < 4; i++)
+            {
+                if (mapas[i].getCalidad() > mapAux.getCalidad())
+                {
+                    mapAux = mapas[i];
+                }
+            }
+
+            for (int i=0; i < 4; i++)
+            {
+                for (int l = 0; l < 3; l++)
+                {
+                    hijos[i].CorregirMapa();
+                }
+                hijos[i].ObtenerCalidad();
+                mapas[i] = hijos[i];
+                mapas[i].ObtenerCalidad();
+            }
+
+            int iAux=0;
+            for (int i = 1; i < 4; i++)
+            {
+                if (mapas[i].getCalidad() < mapas[iAux].getCalidad())
+                {
+                    iAux = i;
+                }
+            }
+
+            if(mapas[iAux].getCalidad() < mapAux.getCalidad())
+            {
+                mapas[iAux] = mapAux;
+            }   
+        }
+
+        map = mapas[0];
+        for (int i = 1; i < 4; i++)
+        {
+            if (mapas[i].getCalidad() > map.getCalidad())
+            {
+                map = mapas[i];
+            }
+        }
+
+        DibujarMapa(map,inTile,outTile);
+        return map;
+    }
+
+    void ReprodudirMapas(Mapa hijo1, Mapa hijo2)
+    {
+        int[,] mapaAux= new int[alto,ancho];
+        int anchoR = randomNum.Next(0, ancho - 1);
+        int altoR = randomNum.Next(0, alto - 1);
+
+        if (randomNum.Next(1, 100) < posCru)
+        {
+            for(int i = 0; i < alto; i++)
+            {
+                for(int j = 0; j< ancho; j++)
+                {
+                    if (i > altoR || (i == altoR && j > anchoR))
+                    {
+                        mapaAux[i, j] = hijo1.getPuntoMapa(i, j);
+                        hijo1.setPuntoMapa(i, j, hijo2.getPuntoMapa(i, j));
+                        hijo2.setPuntoMapa(i, j, mapaAux[i, j]);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < alto; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                if(randomNum.Next(1, 100) < posMut)
+                    hijo1.setPuntoMapa(i, j, randomNum.Next(0,1));
+                if (randomNum.Next(1, 100) < posMut)
+                    hijo2.setPuntoMapa(i, j, randomNum.Next(0, 1));
+            }
+        }
+        hijo1.ObtenerCalidad();
+        hijo2.ObtenerCalidad();
+    }
+
+    public void DibujarMapa(Mapa mapa,GameObject inTile, GameObject outTile)
+    {
+        Transform mapIm = new GameObject("Mapa").transform;
+        for (int i = 0; i < alto; i++)
+            for (int j = 0; j < ancho; j++)
+            {
+                GameObject toInstanciate = mapa.getPuntoMapa(i, j) == 1 ? inTile : outTile;
+                GameObject intance = Instantiate(toInstanciate, new Vector3(i - (alto / 2), j - (ancho / 2), 1f), Quaternion.identity) as GameObject;
+                intance.transform.SetParent(mapIm);
+            }
+    }
+}
